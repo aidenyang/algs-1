734c677bccee607df95ac5ebd8a2461a34bdcf3e
Aiden <aiden.yang@intel.com>
2017-05-05 16:07:02 -0700

    some work on the deque

--- Deque/src/Deque.java
+++ Deque/src/Deque.java
@@ -6,8 +6,9 @@ public class Deque<Item> implements Iterable<Item> {
     private int size; 
     
     private class Node {
-        Item content;
-        Node next;
+        Item content = null;
+        Node next = null;
+        Node previous = null;
     }
     
     public Deque() {
@@ -17,8 +18,7 @@ public class Deque<Item> implements Iterable<Item> {
     }
     
     public boolean isEmpty() {
-        if (first == null) return true;
-        return false;
+        return first == null;
     }
     
     public int size() {
@@ -26,7 +26,42 @@ public class Deque<Item> implements Iterable<Item> {
     }
     
     public void addFirst(Item item) {
-        
+        Node oldFirst = first;
+        first.content = item;
+        first.next = oldFirst;
+        first.previous = null;
+        oldFirst.previous = first;
+        if (isEmpty()) last = null;
+        size++;
+    }
+    
+    public void addLast(Item item) {
+        Node oldLast = last;
+        last.content = item;
+        last.next = null;
+        last.previous = oldLast;
+        if (isEmpty()) first = last;
+        else oldLast.next = last;
+        size++;
+    }
+    
+    public Item removeFirst() {
+        Node item = first;
+        first = item.next;
+        first.previous = null;
+        size--;
+        if (isEmpty()) last = null;
+        return (Item) item;
+
+    }
+    
+    public Item removeLast() {
+        Node item = last;
+        Node prev = last.previous.previous;
+        last = last.previous;
+        last.previous = prev;
+        size--;
+        return (Item) item;
     }
 
     @Override
